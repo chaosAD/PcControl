e3357bf219d33d118479993439169b27dabf0d91
kaosad <frasker@hotmail.com>
2014-09-29 16:37:52 +0800

    Fix 18c.h file to work both on MPLAB and MPLABX

--- src/18c.h
+++ src/18c.h
@@ -1,9 +1,10 @@
 #ifndef _18C_H
 #define _18C_H
 
-#ifdef __18CXX
-//    #include <P18F4520.h>
+#if defined(__XC)
     #include <xc.h>
+#elif defined(__18CXX)
+    #include <P18F4520.h>
 #else
     #include "p18f4520.h"
 #endif
